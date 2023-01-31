377d248213c3659bf370561c44159533833afdfe
Jamieson Walker <Chaymee@users.noreply.github.com>
2023-01-30 21:36:20 -0600

    Removed old store and retrieve code

--- src/options/options.js
+++ src/options/options.js
@@ -1,24 +1,9 @@
-function old_save_options() {
-  localStorage.setItem('solaceURL', document.getElementById('solaceURL').value);
-  localStorage.setItem('solaceVpnName', document.getElementById('vpnName').value);
-  localStorage.setItem('solaceUserName', document.getElementById('userName').value);
-  localStorage.setItem('solacePassword', document.getElementById('password').value);
-  
-}
 function save_options() {
   chrome.storage.local.set({'solaceURL':document.getElementById('solaceURL').value});
   chrome.storage.local.set({'solaceVpnName':document.getElementById('vpnName').value});
   chrome.storage.local.set({'solaceUserName':document.getElementById('userName').value});
   chrome.storage.local.set({'solacePassword':document.getElementById('password').value});
-}
 
-function old_restore_options() {
-  document.getElementById('solaceURL').value = localStorage.getItem('solaceURL') || '';
-  document.getElementById('vpnName').value = localStorage.getItem('solaceVpnName') || '';
-  document.getElementById('userName').value = localStorage.getItem('solaceUserName') || '';
-  document.getElementById('password').value = localStorage.getItem('solacePassword') || '';
-  
-}
 function restore_options() {
   chrome.storage.local.get(["solaceURL"]).then((result) => {
     if (result.solaceURL) {
