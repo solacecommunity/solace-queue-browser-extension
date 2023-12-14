47ed3b08e9b4794fef461b1a12c0ad357843f2d8
Leonard Phillips <leonardjohnphillips@gmail.com>
2023-12-14 15:48:15 +0100

    Added Copy to Clipboard button to queue UI Added error handling for missing Option parameters

--- src/options/options.js
+++ src/options/options.js
@@ -1,32 +1,41 @@
+// [LeoPhillips][02-11-23][v3][START]
 function save_options() {
-  chrome.storage.local.set({'solaceURL':document.getElementById('solaceURL').value});
-  chrome.storage.local.set({'solaceVpnName':document.getElementById('vpnName').value});
-  chrome.storage.local.set({'solaceUserName':document.getElementById('userName').value});
-  chrome.storage.local.set({'solacePassword':document.getElementById('password').value});
-}
+  var loginDetails = {
+    solaceURL: null,
+    solaceVpnName: null,
+    solaceUserName: null,
+    solacePassword: null,
+  }
 
+  loginDetails.solaceURL = document.getElementById('solaceURL').value;
+  loginDetails.solaceVpnName = document.getElementById('vpnName').value;
+  loginDetails.solaceUserName = document.getElementById('userName').value;
+  loginDetails.solacePassword = document.getElementById('password').value;
+  
+  chrome.storage.local.set({'loginDetails': loginDetails});
+}
+// [LeoPhillips][02-11-23][v3][END]
+
+
+// [LeoPhillips][02-11-23][v3][START]
 function restore_options() {
-  chrome.storage.local.get(["solaceURL"]).then((result) => {
-    if (result.solaceURL) {
-      document.getElementById('solaceURL').value = result.solaceURL;
+  chrome.storage.local.get(["loginDetails"]).then((result) => {
+    if (result.loginDetails.solaceURL) {
+      document.getElementById('solaceURL').value = result.loginDetails.solaceURL;
     }
-  });
-  chrome.storage.local.get(["solaceVpnName"]).then((result) => {
-    if(result.solaceVpnName) {
-      document.getElementById('vpnName').value = result.solaceVpnName;
+    if (result.loginDetails.solaceVpnName) {
+      document.getElementById('vpnName').value = result.loginDetails.solaceVpnName;
     }
-  });
-  chrome.storage.local.get(["solaceUserName"]).then((result) => {
-    if(result.solaceUserName) {
-      document.getElementById('userName').value = result.solaceUserName;
+    if (result.loginDetails.solaceUserName) {
+
+      document.getElementById('userName').value = result.loginDetails.solaceUserName;
     }
-  });
-  chrome.storage.local.get(["solacePassword"]).then((result) => {
-    if(result.solacePassword) {
-      document.getElementById('password').value = result.solacePassword;
+    if (result.loginDetails.solacePassword) {
+      document.getElementById('password').value = result.loginDetails.solacePassword;
     }
   });
 }
+// [LeoPhillips][02-11-23][v3][END]
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
